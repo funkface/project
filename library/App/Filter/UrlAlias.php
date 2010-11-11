5a1ffbcb7ea75a321e57e6ffc1c2de28e5af8bb2
Martin Shopland <github@sleepistheenemy.co.uk>
2010-11-11 00:11:53 +0000

    Split App off into submodule.

--- library/App/Filter/UrlAlias.php
+++
@@ -1,9 +0,0 @@
-<?php
-class App_Filter_UrlAlias implements Zend_Filter_Interface
-{
-    public function filter($value)
-    {
-        return strtolower(preg_replace(array('/[^\w\s\-_]+|^[\s\-_]+|[\s\-_]+$/', '/[\s\-_]+/'), array('', '-'), $value));
-    }
-}
-?>
--- library/App/Tool/Provider.php
+++
@@ -1,8 +0,0 @@
-<?php
-class App_Tool_AppDbProvider implements Zend_Tool_Framework_Provider_Interface
-{
-	public function greet()
-	{
-		echo 'Hello from AppDbProvider!';
-	}
-}
